7613c19d26488f36b2bc9f8c395b71d4ec9137a9
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-13 16:18:52 +0200

    cleanup and added github workflow

--- registry.mjs
+++ registry.mjs
@@ -1,4 +1,4 @@
-export { GUID, Specs, VERBOSE, existsSync, extname, fileURLToPath, join, readFileSync, registerSchema, validateSchema } from 'utils';
+export { GUID, Specs } from 'utils';
 export { Container } from './lib/container.mjs';
 export { CtorArgs, CtorArgsRegistry } from './lib/ctor.args.mjs';
 
