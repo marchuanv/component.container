034f73fd0765b8b23ef8b9a213127f43a95ac744
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-13 17:08:54 +0200

    cleanup, renaming and moving

--- registry.mjs
+++ registry.mjs
@@ -1,4 +1,8 @@
+import { Metadata } from './lib/metadata.mjs';
 export { GUID, Specs } from 'utils';
 export { Container } from './lib/container.mjs';
-export { CtorArgs, CtorArgsRegistry } from './lib/ctor.args.mjs';
+export { CtorArgsRegistry } from './lib/ctor-args-registry.mjs';
+export { CtorArgs } from './lib/ctor-args.mjs';
+export { Property } from './lib/property.mjs';
+export { Metadata };
 
