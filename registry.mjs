bcea653af28caadb22a65dc0639fad2dbca2c9cf
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-21 16:11:11 +0200

    Namespace is now a sealed class

--- registry.mjs
+++ registry.mjs
@@ -1,14 +1,11 @@
+import { Reference } from 'component.reference';
 import { Properties } from './lib/properties.mjs';
-import { TypeRegistry } from './lib/type-registry.mjs';
-export { Reference } from 'component.reference';
 export { Specs } from 'component.specs';
 export { Type } from 'component.type';
 export { GUID } from 'component.uuid';
 export { Reflection, base64ToString, stringToBase64 } from 'utils';
-export { ContainerRegistry } from './lib/container-registry.mjs';
 export { Container } from './lib/container.mjs';
 export { CtorArgs } from './lib/ctor-args.mjs';
 export { Property } from './lib/property.mjs';
-export { Registry } from './lib/registry.mjs';
-export { Properties, TypeRegistry };
+export { Properties, Reference };
 
--- specs/index.mjs
+++ specs/index.mjs
@@ -1,18 +1,10 @@
-import { Specs, TypeRegistry } from "../registry.mjs";
+import { Specs } from "../registry.mjs";
 import { AnimalCtorArgs } from "./classes/animal-ctor-args.mjs";
 import { Animal } from "./classes/animal.mjs";
 import { DogCtorArgs } from "./classes/dog-ctor-args.mjs";
 import { Dog } from "./classes/dog.mjs";
 import { FoodCtorArgs } from "./classes/food-ctor-args.mjs";
 import { Food } from "./classes/food.mjs";
-TypeRegistry.register([
-    { namespace: 'common.container', Class: AnimalCtorArgs, isSingleton: false },
-    { namespace: 'common.container', Class: Animal, isSingleton: false },
-    { namespace: 'common.container', Class: DogCtorArgs, isSingleton: false },
-    { namespace: 'common.container', Class: Dog, isSingleton: false },
-    { namespace: 'common.container', Class: FoodCtorArgs, isSingleton: false },
-    { namespace: 'common.container', Class: Food, isSingleton: false }
-]);
 const specs = new Specs(60000, './');
 specs.run();
 export { Animal, AnimalCtorArgs, Dog, DogCtorArgs, Food, FoodCtorArgs };
