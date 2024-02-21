0a76788798a664e917093b3a42bd677e69054f2a
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-21 13:02:49 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,6 +1,10 @@
 import { Properties } from './lib/properties.mjs';
 import { TypeRegistry } from './lib/type-registry.mjs';
-export { GUID, Reference, Reflection, Specs, base64ToString, stringToBase64 } from 'utils';
+export { Reference } from 'component.reference';
+export { Specs } from 'component.specs';
+export { Type } from 'component.type';
+export { GUID } from 'component.uuid';
+export { Reflection, base64ToString, stringToBase64 } from 'utils';
 export { ContainerRegistry } from './lib/container-registry.mjs';
 export { Container } from './lib/container.mjs';
 export { CtorArgs } from './lib/ctor-args.mjs';
--- specs/index.mjs
+++ specs/index.mjs
@@ -5,16 +5,14 @@ import { DogCtorArgs } from "./classes/dog-ctor-args.mjs";
 import { Dog } from "./classes/dog.mjs";
 import { FoodCtorArgs } from "./classes/food-ctor-args.mjs";
 import { Food } from "./classes/food.mjs";
-
 TypeRegistry.register([
-    AnimalCtorArgs,
-    Animal,
-    DogCtorArgs,
-    Dog,
-    FoodCtorArgs,
-    Food
+    { namespace: 'common.container', Class: AnimalCtorArgs, isSingleton: false },
+    { namespace: 'common.container', Class: Animal, isSingleton: false },
+    { namespace: 'common.container', Class: DogCtorArgs, isSingleton: false },
+    { namespace: 'common.container', Class: Dog, isSingleton: false },
+    { namespace: 'common.container', Class: FoodCtorArgs, isSingleton: false },
+    { namespace: 'common.container', Class: Food, isSingleton: false }
 ]);
-
 const specs = new Specs(60000, './');
 specs.run();
 export { Animal, AnimalCtorArgs, Dog, DogCtorArgs, Food, FoodCtorArgs };
