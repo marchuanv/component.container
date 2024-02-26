ddcdb2a88dadff30908e9b1ce293b32069d49448
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-26 15:17:46 +0200

    starting fresh

--- registry.mjs
+++ registry.mjs
@@ -1,13 +1,9 @@
+import { Property } from 'component.property';
 import { Reference } from 'component.reference';
-import { Properties } from './lib/properties.mjs';
 export { Specs } from 'component.specs';
 export { Type } from 'component.type';
-export { GUID } from 'component.uuid';
 export { Reflection, base64ToString, stringToBase64 } from 'utils';
-export { Config } from './lib/config.mjs';
 export { Container } from './lib/container.mjs';
 export { CtorArgs } from './lib/ctor-args.mjs';
-export { Property } from './lib/property.mjs';
-export { Register } from './lib/register.mjs';
-export { Properties, Reference };
+export { Property, Reference };
 
--- specs/index.mjs
+++ specs/index.mjs
@@ -1,17 +1,11 @@
-import { Register, Specs } from "../registry.mjs";
-import { AnimalConfig } from "./classes/animal-config.mjs";
+import { Specs } from "../registry.mjs";
 import { AnimalCtorArgs } from "./classes/animal-ctor-args.mjs";
 import { Animal } from "./classes/animal.mjs";
-import { DogConfig } from "./classes/dog-config.mjs";
 import { DogCtorArgs } from "./classes/dog-ctor-args.mjs";
 import { Dog } from "./classes/dog.mjs";
-import { FoodConfig } from "./classes/food-config.mjs";
 import { FoodCtorArgs } from "./classes/food-ctor-args.mjs";
 import { Food } from "./classes/food.mjs";
-Register.add(AnimalConfig);
-Register.add(DogConfig);
-Register.add(FoodConfig);
 const specs = new Specs(60000, './');
 specs.run();
-export { Animal, AnimalConfig, AnimalCtorArgs, Dog, DogConfig, DogCtorArgs, Food, FoodConfig, FoodCtorArgs };
+export { Animal, AnimalCtorArgs, Dog, DogCtorArgs, Food, FoodCtorArgs };
 
