f906b442dffc68787fc4602b056d2629aeee7bfb
marchuanv <marchuanv@gmail.com>
2024-03-19 01:00:13 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,7 +1,7 @@
 export { PropertiesReference } from 'component.property';
 export { Reference } from 'component.reference';
 export { Specs } from 'component.specs';
-export { TypeReference } from 'component.type';
+export { TypeReference, TypeRegisterEntry, TypeReferenceContext } from 'component.type';
 export { Reflection, base64ToString, stringToBase64 } from 'utils';
 export { Container } from './lib/container.mjs';
 export { CtorArgs } from './lib/ctor-args.mjs';
