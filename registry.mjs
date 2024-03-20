902333c16865e4da7dfa76d57d3e124d676c8011
marchuanv <marchuanv@gmail.com>
2024-03-20 02:07:57 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,5 +1,5 @@
 export { PropertiesReference } from 'component.property';
-export { Reference, ReferenceStack } from 'component.reference';
+export { Reference } from 'component.reference';
 export { Specs } from 'component.specs';
 export { ReferenceId, TypeReference, TypeReferenceContext, TypeRegisterEntry } from 'component.type';
 export { Store, Type, TypeRegister } from 'component.uuid';
