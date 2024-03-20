96ffcdfea2752b54071dca2fc937129eff74c0ce
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-03-20 09:18:05 +0200

    refactor

--- specs/properties-spec.mjs
+++ specs/properties-spec.mjs
@@ -8,41 +8,46 @@ import {
 fdescribe('when container properties change', () => {
     it('should sync data', () => {
 
-        Container.register(Food);
+        try {
+            Container.registerType(Food);
 
-        const expectedName = 'Parody';
-        const expectedAge = 5;
+            const expectedName = 'Parody';
+            const expectedAge = 5;
 
-        const foodArgs = Container.create(FoodCtorArgs, FoodCtorArgs.prototype);
-        foodArgs.isAdultFood = true;
-        foodArgs.name = 'epol';
+            const foodArgs = Container.create(FoodCtorArgs, FoodCtorArgs.prototype);
+            foodArgs.isAdultFood = true;
+            foodArgs.name = 'epol';
 
-        const dogArgs = Container.create(DogCtorArgs, DogCtorArgs.prototype);
-        dogArgs.age = expectedAge;
-        dogArgs.name = expectedName;
-        dogArgs.food = Container.create(Food, FoodCtorArgs.prototype, foodArgs);
-        dogArgs.type = 'dog';
-        dogArgs.weight = 24;
-        dogArgs.vaccinationYears = ['2010', '2011', '2012'];
+            const dogArgs = Container.create(DogCtorArgs, DogCtorArgs.prototype);
+            dogArgs.age = expectedAge;
+            dogArgs.name = expectedName;
+            dogArgs.food = Container.create(Food, FoodCtorArgs.prototype, foodArgs);
+            dogArgs.type = 'dog';
+            dogArgs.weight = 24;
+            dogArgs.vaccinationYears = ['2010', '2011', '2012'];
 
-        const dog = Container.create(Dog, DogCtorArgs.prototype, dogArgs);
-        expect(dog.name).toBe(expectedName);
-        expect(dog.age).toBe(expectedAge);
+            const dog = Container.create(Dog, DogCtorArgs.prototype, dogArgs);
+            expect(dog.name).toBe(expectedName);
+            expect(dog.age).toBe(expectedAge);
 
-        let fireCount = 0;
-        dog.onSet({ name: null }, String, (value) => {
-            fireCount = fireCount + 1;
-            return expectedName;
-        });
-        dog.onSet({ age: null }, Number, (value) => {
-            fireCount = fireCount + 1;
-            return expectedAge;
-        });
-        dog.age = 25; //onChange
-        dog.name = 'Lassy'; //onChange
+            let fireCount = 0;
+            dog.onSet({ name: null }, String, (value) => {
+                fireCount = fireCount + 1;
+                return expectedName;
+            });
+            dog.onSet({ age: null }, Number, (value) => {
+                fireCount = fireCount + 1;
+                return expectedAge;
+            });
+            dog.age = 25; //onChange
+            dog.name = 'Lassy'; //onChange
 
-        expect(fireCount).toBe(2);
-        expect(dog.name).toBe(expectedName);
-        expect(dog.age).toBe(expectedAge);
+            expect(fireCount).toBe(2);
+            expect(dog.name).toBe(expectedName);
+            expect(dog.age).toBe(expectedAge);
+        } catch (error) {
+            console.log(error);
+            fail('did not expect errors.')
+        }
     });
 });
